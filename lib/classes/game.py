6dd62b9206cac37b6f3cdca7d56ff6b6dc87d819
Carla <113267384+RuixDsgn@users.noreply.github.com>
2023-06-28 15:44:00 -0700

    completed mock challenge with all tests passing

--- lib/classes/game.py
+++ lib/classes/game.py
@@ -1,16 +1,32 @@
 class Game:
     def __init__(self, title):
         self.title = title
+        # if hasattr(self, "name"):
+        #     raise Exception()
         self._results = []
         self._players = []
+
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, title):
+        if (hasattr(self, "title")):
+            raise Exception()
+        elif (type(title) == str) and len(title) > 0:
+            self._title = title
+        else:
+            raise Exception
         
     def results(self, new_result=None):
         from classes.result import Result
-        pass
+        return [result for result in Result.all if self == result.game]
     
     def players(self, new_player=None):
-        from classes.player import Player
-        pass
+        players = [result.player for result in self.results()]
+        return players
     
     def average_score(self, player):
-        pass
+        scores = [result.score for result in self.results()]
+        return sum(scores) / len(scores)
--- lib/classes/player.py
+++ lib/classes/player.py
@@ -6,20 +6,39 @@ class Player:
         self.username = username
         self._results = []
         self._games_played = []
+    
+    @property
+    def username(self):
+        return self._username
+    
+    @username.setter
+    def username(self, username):
+        if (type(username) == str) and (2 <= len(username) <= 16):
+            self._username = username
+        else:
+            raise Exception()
+
         
     def results(self, new_result=None):
         from classes.result import Result
-        pass
+        return [result for result in Result.all if self == result.player]
     
     def games_played(self, new_game=None):
-        from classes.game import Game
-        pass
+        games = [result.game for result in self.results()]
+        return games
     
     def played_game(self, game):
-        pass
+        for result in self.results():
+            if (game == result.game):
+                return True
+        return False
     
     def num_times_played(self, game):
-        pass
+        play_count = 0
+        for result in self.results():
+            if (game == result.game):
+                play_count += 1
+        return play_count
     
     @classmethod
     def highest_scored(cls, game):
